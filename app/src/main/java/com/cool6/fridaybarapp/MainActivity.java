29bf7c1d0f93f2de00504fdaaa4a1214897359db
liviu-lesan <liviu-lesan@github.com>
2019-04-07 02:33:48 +0200

    mobilepay

--- app/src/main/java/com/cool6/fridaybarapp/MainActivity.java
+++ app/src/main/java/com/cool6/fridaybarapp/MainActivity.java
@@ -3,11 +3,21 @@ package com.cool6.fridaybarapp;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.net.HttpURLConnection;
+
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void createBinding(){
+        OkHttpClient client = new OkHttpClient();
+        String URL = "ttps://api.sandbox.mobilepay.dk/bindings-restapi/api/v1/bindings";
+
+
     }
 }
